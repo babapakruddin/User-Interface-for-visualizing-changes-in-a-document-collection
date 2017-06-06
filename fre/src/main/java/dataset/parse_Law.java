32212f7b6b3f78eaae92728a0486c0748e9f2c2a
Baba pakruddin Tailor <baba.tailor@st.ovgu.de>
2017-06-06 18:02:41 +0200

    Update parse_Law.java

--- fre/src/main/java/dataset/parse_Law.java
+++ fre/src/main/java/dataset/parse_Law.java
@@ -1,16 +1,78 @@
-package dataset;
+package org.std.the.law.app;
+
+import java.util.ArrayList;
+
+import org.primefaces.model.chart.BarChartModel;
 
 public class parse_Law {
 	public String doknr;
 	public String builddate;
-
 	public String norm;
-	   
-	parse_Law(String a,String b, String c)
+	private String fplag;
+	private String standkommentar;
+	private String ABWFORMAT;
+	
+	private String completeext;
+	private String diffHtml;
+	private String curInd;
+	
+	private String dffNumber;
+	
+	
+	ArrayList<result_law> resultLaw = new ArrayList<result_law>();
+	
+	private BarChartModel perLwBarMD;
+	
+	public ArrayList<result_law> getResultLaw() {
+		return resultLaw;
+	}
+
+
+	public void setResultLaw(ArrayList<result_law> resultLaw) {
+		this.resultLaw = resultLaw;
+	}
+
+
+	public String getShowChnages() {
+		return showChnages;
+	}
+
+
+	public void setShowChnages(String showChnages) {
+		this.showChnages = showChnages;
+	}
+
+
+	private String changesExists;
+	private String showChnages;
+	
+	private boolean disableFlag;
+	
+	public boolean isDisableFlag() {
+		return disableFlag;
+	}
+
+
+	public void setDisableFlag(boolean disableFlag) {
+		this.disableFlag = disableFlag;
+	}
+
+
+	parse_Law(String bDate, String fpF ,String dok, String nOR,  String st, String cpText)
 	{
-		 setDoknr(a);
-		 setBuilddate(b);
-		 setNorm(c);
+		 
+		 setBuilddate(bDate);
+		 setfplag(fpF);
+		 setDoknr(dok);
+		 
+		 setNorm(nOR);
+		 setStandkommentar(st);
+		 setCompleteext(cpText);
+	}
+
+
+	public parse_Law() {
+		// TODO Auto-generated constructor stub
 	}
 
 
@@ -41,5 +103,99 @@ public class parse_Law {
 	public void setBuilddate(String builddate) {
 		this.builddate = builddate;
 	}
+
+
+
+	public String getfplag() {
+		return fplag;
+	}
+
+
+	public void setfplag(String fplag) {
+		this.fplag = fplag;
+	}
+
+
+	public String getABWFORMAT() {
+		return ABWFORMAT;
+	}
+
+
+	public void setABWFORMAT(String aBWFORMAT) {
+		ABWFORMAT = aBWFORMAT;
+	}
+
+
+	public String getStandkommentar() {
+		return standkommentar;
+	}
+
+
+	public void setStandkommentar(String standkommentar) {
+		this.standkommentar = standkommentar;
+	}
+
+
+	public String getCompleteext() {
+		return completeext;
+	}
+
+
+	public void setCompleteext(String completeext) {
+		this.completeext = completeext;
+	}
+
+
+	public String getDiffHtml() {
+		return diffHtml;
+	}
+
+
+	public void setDiffHtml(String diffHtml) {
+		this.diffHtml = diffHtml;
+	}
+
+
+	public String getChangesExists() {
+		return changesExists;
+	}
+
+
+	public void setChangesExists(String changesExists) {
+		this.changesExists = changesExists;
+	}
+
+
+	public String getCurInd() {
+		return curInd;
+	}
+
+
+	public void setCurInd(String curInd) {
+		this.curInd = curInd;
+	}
+
+
+	public BarChartModel getPerLwBarMD() {
+		return perLwBarMD;
+	}
+
+
+	public void setPerLwBarMD(BarChartModel perLwBarMD) {
+		this.perLwBarMD = perLwBarMD;
+	}
+
+
+	public String getDffNumber() {
+		return dffNumber;
+	}
+
+
+	public void setDffNumber(String dffNumber) {
+		this.dffNumber = dffNumber;
+	}
+
+
+
 
 }
